dc4fd913e702d54521751405d096a927c0ad6449
Ithrasil <pawel186@onet.eu>
2018-06-28 13:36:51 +0200

    added saveMultiple declaration

--- 11-database-performance/src/IRepository.php
+++ 11-database-performance/src/IRepository.php
@@ -3,5 +3,6 @@
 interface IRepository
 {
     public function save(Transaction $transaction): void;
+    public function saveMultiple(string $table, array $transactions, array $fields) : void;
     public function get(string $id) : Transaction;
 }
